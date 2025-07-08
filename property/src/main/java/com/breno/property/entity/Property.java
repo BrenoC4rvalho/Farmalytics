a90acf856c32af69430b3512dad8de7df9c019d1
BrenoC4rvalho <bc101004@gmail.com>
2025-07-08 19:04:33 -0300

    feat(property): create property entity.

---
+++ property/src/main/java/com/breno/property/entity/Property.java
@@ -0,0 +1,16 @@
+package src.main.java.com.breno.property.entity;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class Property {
+
+    private UUID id;
+    private String name;
+    private String addressCity;
+    private String addressState;
+    private String totalArea;
+    private LocalDateTime creatAt;
+    private LocalDateTime updateAt;
+
+}
