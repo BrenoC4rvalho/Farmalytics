9b584ccf8174b6fdcebb7bc5a047ccccd6338aaa
BrenoC4rvalho <bc101004@gmail.com>
2025-07-07 21:56:24 -0300

    feat: create microservice from Iot Data Service..

---
+++ IoTDataService/src/main/java/com/breno/IoTDataService/IoTDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.breno.IoTDataService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoTDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IoTDataServiceApplication.class, args);
+	}
+
+}
---
+++ IoTDataService/src/test/java/com/breno/IoTDataService/IoTDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.breno.IoTDataService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoTDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
