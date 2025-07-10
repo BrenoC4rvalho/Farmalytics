d678eb2070bbcedcb9423ef1ca82e16e6e1d5c8f
BrenoC4rvalho <bc101004@gmail.com>
2025-07-10 18:46:03 -0300

    feat: create getters, setters and validations in field entity.

--- property/src/main/java/com/breno/property/entity/Field.java
+++ property/src/main/java/com/breno/property/entity/Field.java
@@ -1,19 +1,46 @@
-package src.main.java.com.breno.property.entity;
+package com.breno.property.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.awt.*;
 import java.math.BigDecimal;
+import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "fields")
 public class Field {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
+
+    @Column(nullable = false)
     private String identifier;
+
+    @Column(nullable = false, precision = 10, scale = 2)
     private BigDecimal area;
+
+    @Column(nullable = false, columnDefinition = "geometry(Polygon, 4326)")
     private Polygon geometry;
+
     private String currentCrop;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "property_id", nullable = false)
     private Property property;
-    private LocalDateTime createdAt;
-    private LocalDateTime updateAt;
+
+    @Column(updatable = false)
+    private Instant createdAt;
+
+    private Instant updatedAt;
 
 }
