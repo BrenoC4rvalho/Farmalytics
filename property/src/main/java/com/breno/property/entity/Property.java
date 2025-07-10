9dedf47911f9958af160a464f08b5a63a1ec0e09
BrenoC4rvalho <bc101004@gmail.com>
2025-07-10 18:46:38 -0300

    feat: create getters, setters and validation in property entity.

--- property/src/main/java/com/breno/property/entity/Property.java
+++ property/src/main/java/com/breno/property/entity/Property.java
@@ -1,16 +1,45 @@
-package src.main.java.com.breno.property.entity;
+package com.breno.property.entity;
 
-import java.time.LocalDateTime;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.List;
 import java.util.UUID;
 
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "properties")
 public class Property {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private String addressCity;
+
+    @Column(nullable = false, length = 2)
     private String addressState;
-    private String totalArea;
-    private LocalDateTime creatAt;
-    private LocalDateTime updateAt;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal totalArea;
+
+    @Column(updatable = false)
+    private Instant createdAt;
+
+    private Instant updatedAt;
+
+    @OneToMany(mappedBy = "property", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Field> fields;
 
 }
