616cd045bae694e818a735e5ac68716f29db6c08
BrenoC4rvalho <bc101004@gmail.com>
2025-07-12 20:20:48 -0300

    feat(property): create service for property.

---
+++ property/src/main/java/com/breno/property/service/PropertyService.java
@@ -0,0 +1,52 @@
+package com.breno.property.service;
+
+import com.breno.property.dto.CreatePropertyRequestDTO;
+import com.breno.property.dto.PropertyResponseDTO;
+import com.breno.property.entity.Property;
+import com.breno.property.repository.PropertyRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class PropertyService {
+
+    private final PropertyRepository propertyRepository;
+
+    public PropertyService(PropertyRepository propertyRepository) {
+        this.propertyRepository = propertyRepository;
+    }
+
+    public PropertyResponseDTO createProperty(CreatePropertyRequestDTO requestDTO) {
+        Property property = Property.builder()
+                .name(requestDTO.getName())
+                .addressCity(requestDTO.getAddressCity())
+                .addressState(requestDTO.getAddressState())
+                .totalArea(requestDTO.getTotalArea())
+                .createdAt(Instant.now())
+                .updatedAt(Instant.now())
+                .build();
+
+        Property savedProperty = propertyRepository.save(property);
+
+        return mapToResponseDTO(savedProperty);
+    }
+
+    public List<PropertyResponseDTO> getAllProperties() {
+        return propertyRepository.findAll()
+                .stream()
+                .map(this::mapToResponseDTO)
+                .collect(Collectors.toList());
+    }
+
+    private PropertyResponseDTO mapToResponseDTO(Property property) {
+        return PropertyResponseDTO.builder()
+                .id(property.getId())
+                .name(property.getName())
+                .build();
+    }
+
+}
