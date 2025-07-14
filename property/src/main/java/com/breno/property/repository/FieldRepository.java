6c231fadf8cda31aa0095c6db9918779016cffba
BrenoC4rvalho <bc101004@gmail.com>
2025-07-14 20:44:43 -0300

    feat(property): edit field repository.

--- property/src/main/java/com/breno/property/repository/FieldRepository.java
+++ property/src/main/java/com/breno/property/repository/FieldRepository.java
@@ -3,7 +3,9 @@ package com.breno.property.repository;
 import com.breno.property.entity.Field;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface FieldRepository extends JpaRepository<Field, UUID> {
+    List<Field> findAllByPropertyId(UUID propertyId);
 }
