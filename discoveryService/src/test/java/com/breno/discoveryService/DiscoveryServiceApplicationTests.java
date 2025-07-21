d86b8badd3fe2210c7369e629cdf7810545c5485
BrenoC4rvalho <bc101004@gmail.com>
2025-07-21 19:44:36 -0300

    feat(discoveryService): create discoveryService application.

---
+++ discoveryService/src/test/java/com/breno/discoveryService/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.breno.discoveryService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
